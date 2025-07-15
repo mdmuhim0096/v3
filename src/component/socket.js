7f8c7556d3d2f884d5d1d2235eeb53d0b925b2fa
mdmuhim0096 <muhinking765@gmail.com>
2025-07-15 12:58:00 +0600

    add some code

--- src/component/socket.js
+++ src/component/socket.js
@@ -1,3 +1,6 @@
 import io from "socket.io-client";
-const socket = io("https://node-v1-tc13.onrender.com");
+const socket = io("https://node-v1-tc13.onrender.com", {
+    transports: ["websocket"],
+    withCredentials: true
+});
 export default socket;
